eedcd749695be5b0373748483290e171288a785d
Lorenzo Torsani <lorenzo.torsani@mail.polimi.it>
2023-04-05 09:59:26 +0200

    Prova3

--- Proj_TIW/src/main/java/it/polimi/tiw/project/TestServlet.java
+++ Proj_TIW/src/main/java/it/polimi/tiw/project/TestServlet.java
@@ -37,5 +37,5 @@ public class TestServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-//PALLEPALLEPALLECULOSESSO
+//PALLEPALLEPALLECULOSESSOCAZZO
 }
