9a757f45a5dce409386272766fdd894d94b0f4d1
simone zacchetti <simone.zacchetti@mail.polimi.it>
2023-05-05 12:05:11 +0200

    aggiunto link che porta alla pagina per fare un offerta

--- Proj_TIW/src/main/java/it/polimi/tiw/project/controllers/GoToDettaglioAsta.java
+++ Proj_TIW/src/main/java/it/polimi/tiw/project/controllers/GoToDettaglioAsta.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
